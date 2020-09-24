a0c1b721dd2385f611dad4067f6ccb9a1f38263c
paul_vonhagen <pvonhagen26@gmail.com>
2020-09-23 18:30:29 -0700

    added function to iterate through cards index and display properties from each

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const cardsIndex = "http://127.0.0.1:3000/api/cards"
 
 document.addEventListener('DOMContentLoaded', () => {
+   getCards()
+})
+
+function getCards(){
     fetch(cardsIndex)
     .then(resp => resp.json())
     .then(cards => {
-        console.log(cards);
+        cards.data.forEach(card => {
+            const cardMarkup = `
+            <div data-id=${card.id}>
+            <h3>${card.attributes.question}</h3>
+            </div>
+            <br><br>
+            `;
+
+            document.querySelector('#card-container').innerHTML += cardMarkup
+        })
+        
     })
-})
+}
