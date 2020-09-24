dd87003a160f68cd8d0acba5f3db28807b527cc6
paul_vonhagen <pvonhagen26@gmail.com>
2020-09-23 19:40:39 -0700

    create form handler for card creation form

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const cardsIndex = "http://127.0.0.1:3000/api/cards"
 
 document.addEventListener('DOMContentLoaded', () => {
    getCards()
+   const createCardForm = document.querySelector("#create-card-form")
+   createCardForm.addEventListener("submit", (e) => createFormHandler(e))
 })
 
 function getCards(){
@@ -20,4 +22,16 @@ function getCards(){
         })
         
     })
+}
+
+function createFormHandler(e){
+    e.preventDefault()
+    const questionInput = document.querySelector('#input-question').value
+    const answerInput = document.querySelector('#input-answer').value
+    const deckId = parseInt(document.querySelector('#decks').value)
+    postCard(questionInput, answerInput, deckId)
+}
+
+function postCard(question, answer, deck_id){
+    console.log(question, answer, deck_id)
 }
