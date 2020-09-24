1221a678126b279d2a7ef05c218ad5aaa0d56237
paul_vonhagen <pvonhagen26@gmail.com>
2020-09-23 18:12:08 -0700

    add domcontentloaded event listener and test fetch request to cards index

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("in index.js");
+const cardsIndex = "http://127.0.0.1:3000/api/cards"
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch(cardsIndex)
+    .then(resp => resp.json())
+    .then(cards => {
+        console.log(cards);
+    })
+})
