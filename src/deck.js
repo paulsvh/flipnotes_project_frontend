a77366b62cb4e663878469c32b661172fc0dd3c7
paul_vonhagen <pvonhagen26@gmail.com>
2020-10-01 19:09:41 -0700

    build out deck constructor and renderDeck() function in deck.js

--- src/deck.js
+++ src/deck.js
@@ -1,6 +1,15 @@
 class Deck {
 
+    constructor(deck, deckAttributes){
+        this.id = deck.id
+        this.name = deckAttributes.name
+        this.decks = deckAttributes.cards
+        Deck.all.push(this)
+    }
 
+    renderDeck() {
+        return `<option value="${this.id}>${this.name}</option>`
+    }
 
-    
 }
+Deck.all = [];
